d7ed97148e003e543b58e72fc3eae0362e7e50b8
Vrundathummar <thummarvrunda51@gmail.com>
2024-06-06 14:56:32 +0530

    06-06-24

--- ferme-vrunda/src/componentes/Footer.jsx
+++ ferme-vrunda/src/componentes/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='bg-white mt-40 p-16'>
                 <h1 className='text-teal-900 flex justify-center text-2xl' style={{ fontFamily: 'Cormorant sans-serif' }}>Real Reviews From Real Costumer</h1>
                 <div>
-                <div className='mt-5 px-36'>
+                {/* <div className='mt-5 px-36'> */}
                     <img className='w-20' src={img1} alt="" />
                     <p className='text-xs text-teal-900 ml-5 mt-3 font-semibold'>Exellent</p>
 
